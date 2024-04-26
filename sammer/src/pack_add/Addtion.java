e1bc1c44888438c84d6008fcc00ada255422364e
Samir Pihul <samirpihul27@gmail.com>
2024-04-26 10:58:44 +0530

    Addition

---
+++ sammer/src/pack_add/Addtion.java
@@ -0,0 +1,16 @@
+package pack_add;
+
+import java.util.Scanner;
+
+public class Addtion {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter the a ");
+        a=scan.nextInt();
+        System.out.println("Enter the b ");
+        b=scan.nextInt();
+        c=a+b;
+        System.out.println(c);
+    }
+}
