d3b9306afab14a313bab8a829578de0991d2bef3
Samir Pihul <samirpihul27@gmail.com>
2024-04-26 11:05:34 +0530

    Array Largest No

---
+++ sammer/src/pack_Array/LargestNo.java
@@ -0,0 +1,27 @@
+package pack_Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LargestNo
+{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many elements ");
+        int size = scan.nextInt();
+        int []arr = new int[size];
+
+        System.out.println("Enter array elements ");
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = scan.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println("Largest " + arr[arr.length - 1]);
+
+
+
+    }
+}
