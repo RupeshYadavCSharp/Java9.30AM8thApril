2c7160fd17f2320aba138f0c1eafce6e88ae0d4d
Samir Pihul <samirpihul27@gmail.com>
2024-05-09 18:16:42 +0530

    java

---
+++ sammer/src/pack_Methods/DateTimeCalculator.java
@@ -0,0 +1,17 @@
+package pack_Methods;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class DateTimeCalculator {
+    public static void dob(){
+        Scanner scan=new Scanner (System.in);
+        System.out.println("Enter the year,month,date of brth");
+        int yr=scan.nextInt();
+        int mn=scan.nextInt();
+        int date=scan.nextInt();
+
+        LocalDate dob;
+
+    }
+}
--- sammer/src/pack_Methods/Dob.java
+++ sammer/src/pack_Methods/Dob.java
@@ -1,25 +1,31 @@
 package pack_Methods;
 
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Scanner;
 
 public class Dob {
-    public void DateofBirth(){
+    public  static  void DateofBirth(){
         Scanner scan=new Scanner (System.in);
-        int date,month,year;
+        int Days,month,year;
         int Today=7,t;
         int Month=5,m;
         int Year=2024,y;
-        System.out.println("Enter the Birthday date ");
-        date=scan.nextInt();
+        System.out.println("Enter the date  ");
+        Days=scan.nextInt();
         System.out.println("Enter the month ");
         month=scan.nextInt();
         System.out.println("Enter the year");
         year=scan.nextInt();
-        t=Today-date;
-        m=Month-month;
-        y=Year-year;
-        System.out.println("The todays date of Birth is "+t+m+y);
+
+        LocalDate Dob=LocalDate.of(year,month,Days);
+        LocalDate curr=LocalDate.now();
+
+        Period p=Period.between(Dob,curr);
+        System.out.println("Years "+p.getYears());
+        System.out.println("month "+p.getMonths());
+        System.out.println("day "+p.getDays());
 
 
 
@@ -28,7 +34,7 @@ public class Dob {
     }
 
     public static void main(String[] args) {
-        Dob d=new Dob();
-        d.DateofBirth();
+
+        DateofBirth();
     }
 }
