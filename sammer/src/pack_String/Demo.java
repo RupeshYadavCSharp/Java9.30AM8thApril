f38504e0e8082e065ca5610cb94c5cde80eb872f
Samir Pihul <samirpihul27@gmail.com>
2024-05-22 18:19:11 +0530

    sa

---
+++ sammer/src/pack_String/Demo.java
@@ -0,0 +1,16 @@
+package pack_String;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        String arg[]={"samir","askshu","ayush","Ram"};
+        for (int i = 0; i < arg.length; i++) {
+            char[] arr = new char[0];
+            char out=arr[0];
+
+
+        }
+    }
+}
