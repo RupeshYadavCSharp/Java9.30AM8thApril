82edacfef6007fab4a4b2ed5794b49f8afbcae81
Samir Pihul <samirpihul27@gmail.com>
2024-05-28 18:00:22 +0530

    java

---
+++ sammer/src/pack_classobj/Student.java
@@ -0,0 +1,12 @@
+package pack_classobj;
+
+public class Student {
+    private int rollno,sem;
+    private String name;
+    private String dept;
+    public void setData (int r,int s ,String n,String d){
+
+
+
+    }
+}
