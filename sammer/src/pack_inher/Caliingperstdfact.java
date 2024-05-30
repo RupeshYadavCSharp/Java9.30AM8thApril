580c1c26d9878618334b7f4bf6653fe0dce8a906
Samir Pihul <samirpihul27@gmail.com>
2024-05-30 18:08:43 +0530

    java

--- sammer/src/pack_inher/Caliingperstdfact.java
+++ sammer/src/pack_inher/Caliingperstdfact.java
@@ -1,8 +1,14 @@
 package pack_inher;
 
+import java.util.Scanner;
+
 public class Caliingperstdfact
 {
     public static void main(String[] args) {
+        Student1 objstd=new Student1();
+        objstd.readStd();
+        objstd.ShowStd();
+        Scanner sacned=new Scanner(System.in);
 
     }
 }
