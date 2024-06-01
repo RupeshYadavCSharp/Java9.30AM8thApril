6ce30d74314db38b94e3ad895086114f1a8953a9
Samir Pihul <samirpihul27@gmail.com>
2024-06-01 23:02:01 +0530

    java

---
+++ sammer/src/pack_poly/Addition.java
@@ -0,0 +1,11 @@
+package pack_poly;
+
+import java.util.Scanner;
+
+public class Addition {
+    public void add(int x,int y){
+        Scanner scan=new Scanner(System.in);
+        
+    }
+
+}
--- sammer/src/pack_poly/Areas.java
+++ sammer/src/pack_poly/Areas.java
@@ -2,58 +2,62 @@ package pack_poly;
 
 import java.util.Scanner;
 
-import static javax.print.attribute.standard.MediaSizeName.A;
+//Method Overloading
 
 public class Areas {
-    public void area(double lenghth, double breadth){
-        double A;
+    public void area(double length,double breadth){
+        double A,B;
         Scanner scan=new Scanner(System.in);
-
-        A=scan.nextDouble();
-        A=lenghth*breadth;
-        System.out.println("The area is"+A);
+        System.out.println("Enter the Area length and Breadth ");
+        length=scan.nextDouble();
+        breadth=scan.nextDouble();
+         A=length*breadth;
+        System.out.println("The area is "+A);
     }
-    public void area(double redius){
-        double red;
+    public void area(double radius){
         Scanner scan=new Scanner(System.in);
-
-        red=scan.nextDouble();
-        red=3.14*redius*redius;
-        System.out.println("The redius  is"+red);
-
+        System.out.println("Enter the redius ");
+        radius=scan.nextDouble();
+        double red=3.14*radius*radius;
+        System.out.println("The redius of circule is "+red);
 
 
     }
-    public void area(float base, float height){
+    public void area(float base,float height){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the Area of triangle Base\n Height  ");
+        base=scan.nextFloat();
+        height=scan.nextFloat();
         float tri;
-        Scanner scan=new Scanner(System.in);
-        System.out.println("Enter the area of Triangle ");
-        tri=scan.nextFloat();
         tri=(base*height)/2;
-        System.out.println("The area of traingle is "+tri);
-
-
+        System.out.println("The area of triangle is"+tri);
     }
 
     public static void main(String[] args) {
-        int ch;
         Scanner scan=new Scanner(System.in);
-        System.out.println("Enter the area");
-        System.out.println("Enter the redius ");
-        System.out.println("Enter the area of triangle ");
+        int ch;
+        System.out.println("1 Enter the area\n 2 redius \n 3 area of traingle ");
         ch=scan.nextInt();
-        Areas obj=new Areas();
-
-
+        Areas objare=new Areas();
         switch (ch){
             case 1:
-                obj.area();
-                System.out.println("The area is "+A);
-
+                double l = 0,b=1;
+                objare.area(l,b);
+                break;
+            case 2:
+                double r=0;
+                objare.area(r);
+                break;
+            case 3:
+                float bas=0,he=1;
+                objare.area(bas,he);
+            default:
+                System.out.println("Incorrect number ");
 
         }
 
 
-
     }
+
+
 }
