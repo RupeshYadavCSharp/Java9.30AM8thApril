4abce36ec6eb7a16d035759b61f7d137170d8aed
Samir Pihul <samirpihul27@gmail.com>
2024-06-25 18:53:03 +0530

    java

---
+++ sammer/src/pack_GuiApp/Gui.java
@@ -0,0 +1,17 @@
+package pack_GuiApp;
+
+public class Gui {
+    public Gui()
+    {
+
+
+
+
+        //4background color
+        //2img on the frame
+        //1title
+        //3frame adust full screen
+
+
+    }
+}
